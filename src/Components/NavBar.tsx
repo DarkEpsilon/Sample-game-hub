0cabe22017f6b70f33a9e9feb0f2ef6a3d217959
Light Epsilon <lightepsilon@icloud.com>
2023-07-16 15:39:02 -0400

    Build InputSearch

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,14 +1,16 @@
-﻿import { HStack, Image, Text } from "@chakra-ui/react"
-import logo from '../assets/logo.webp'
-import ColorModeSwitch from "./ColorModeSwitch"
+﻿import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SerachInput from "./SerachInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent='space-between' padding='10px'>
-      <Image src={logo} boxSize='60px' />
-      <ColorModeSwitch/>
+    <HStack padding="10px">
+      <Image src={logo} boxSize="60px" />
+      <SerachInput />
+      <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
